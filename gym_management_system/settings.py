790ef8f91daa25930fa03dd253e5db195c424262
bijeshofficial <bijeshr.kunwar@gmail.com>
2020-08-21 16:39:21 +0545

    hid secret key

--- gym_management_system/settings.py
+++ gym_management_system/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qh07++z@i$yv!@e$(2%fc&a$=!+bkm%%^reao#p7v3qfy$=u7*'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
